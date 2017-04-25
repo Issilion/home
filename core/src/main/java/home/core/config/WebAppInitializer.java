d0410042fc204fc40636cda19c785d31fdc4c03b
MIkhail <MIkhail@MIkhail-Pc>
2017-04-25 07:57:28 +0300

    Добавление юазового спринг секурити

---
+++ core/src/main/java/home/core/config/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package home.core.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{ 
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {MvcConfiguration.class}; // We dont need any special servlet config yet.
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+}
--- core/src/main/java/home/core/controller/HomeController.java
+++ core/src/main/java/home/core/controller/HomeController.java
@@ -6,13 +6,38 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/")
+	@RequestMapping(value="/home")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
 	}
+	
+	@RequestMapping(value = "/protected**", method = RequestMethod.GET)
+	public ModelAndView protectedPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security 3.2.4 Hello World Tutorial");
+		model.addObject("message", "This is protected page - Only for Admin Users!");
+		model.setViewName("protected");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/confidential**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security 3.2.4 Hello World Tutorial");
+		model.addObject("message", "This is confidential page - Need Super Admin Role!");
+		model.setViewName("protected");
+
+		return model;
+
+	}
+
 }
